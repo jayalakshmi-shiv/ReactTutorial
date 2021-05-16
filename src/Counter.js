e98390beeb6f836f00f2fef9b67d659781ee872f
Jayalakshmi S <jayalakshmis692@gmail.com>
2021-05-16 12:37:48 +0530

    react life cycle methods using hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+let updateEveryTime = false;
 
 
 export default class Counter extends React.Component {
@@ -6,11 +7,11 @@ export default class Counter extends React.Component {
     constructor(props) {
         console.log("inside constructor")
         super(props);
-        this.state = { counter: 0}
+        this.state = { counter: 0 }
     }
 
-
-    componentWillMount(){                                  //deprecated
+    // ************************* Mounting Phase ******************************
+    componentWillMount() {                                  //deprecated
         console.log("inside component will mount");
     }
 
@@ -19,6 +20,7 @@ export default class Counter extends React.Component {
     //     return null;
     // }
 
+    // ************************* Update Phase *********************************
     componentDidMount() {
         console.log("inside component did mount");
     }
@@ -33,12 +35,12 @@ export default class Counter extends React.Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log("inside should component update", nextProps, nextState);
-        // if(true) return false;
+        // for not rendering, uncomment first line and comment second line after this
+        // if(true) return false;   
         return true;
     }
 
-    componentWillUpdate(nextProps, nextState){              //deprecated
-
+    componentWillUpdate(nextProps, nextState) {              //deprecated
         console.log("inside component will update", nextProps, nextState);
     }
     // getSnapshotBeforeUpdate(prevProps, prevState) {
@@ -50,46 +52,76 @@ export default class Counter extends React.Component {
         console.log("inside component did update", prevProps, prevState);
     }
 
+    // ************************* un mounting Phase ******************************
+
     componentWillUnmount(){
         console.log("inside component will unmount")
     }
-    
 
     render() {
         console.log("inside render");
-        return(
-                <div>
-                <h1 style={{textAlign:"initial"}}>{this.props.greet}</h1>
+        return (
+            <div>
+                <h1 style={{ textAlign: "initial" }}>{this.props.greet}</h1>
                 <div className="row">
-                <button onClick={() => this.setState({...this.state,counter: this.state.counter - 1 })} className={"btn btn-primary"}>-</button>
-                <span style={{ padding: "10px", fontSize: "30px" }}>{this.state.counter}</span>
-                <button onClick={() => this.setState( { ...this.state,counter: this.state.counter + 1 })} className={"btn btn-primary"}>+</button>
+                    <button onClick={() => this.setState({ ...this.state, counter: this.state.counter - 1 })} className={"btn btn-primary"}>-</button>
+                    <span style={{ padding: "10px", fontSize: "30px" }}>{this.state.counter}</span>
+                    <button onClick={() => this.setState({ ...this.state, counter: this.state.counter + 1 })} className={"btn btn-primary"}>+</button>
                 </div>
             </div>
         )
-
-        
     }
 }
 
 
 
 
-// (-)  10   (+)
 
-
-// const Counter = (props)=>{
+// const Counter = (props) => {
 //     const [counter, setCounter] = useState(0);
-//     console.log("props>>>>>>>>>>>>>>",props);
+//     const [conditionalUpdate, setConditionalUpdate] = useState(false);
+
+//     // ************************* Mounting Phase ******************************
+//     useEffect(() => {
+//         console.log("inside component did mount")
+//     }, [])
 
 
+//     // ************************* Update Phase ********************************
+//     //runs on initial mount and on everyUpdate
+//     useEffect(() => {
+//         console.log("I run on initial mount and on every update")
+//     })
 
-//     return(
-//         <div style={{margin:"auto",width:"50%"}}>
-//          <h1>{props.greet}</h1>
-//           <button onClick={()=>setCounter(counter-1)}>-</button>
-//           {counter}
-//           <button onClick={()=>setCounter(counter+1)}>+</button>
+//     useEffect(() => {
+//         if (updateEveryTime) {
+//             console.log("Component did update");
+//         }
+//         else {
+//             updateEveryTime = true;
+//         }
+//     })
+//     useEffect(() => {
+//         console.log("Update for specific state")
+//     }, [conditionalUpdate])
+
+//     // ************************* un mounting Phase *****************************
+
+//     useEffect(()=>{
+//         return ()=>{console.log("component will unmount")}
+//     },[])
+
+
+//     return (
+//         <div style={{ textAlign: "left" }}>
+//             <h1>{props.greet}</h1>
+//             <button onClick={() => setCounter(counter - 1)} className={"btn btn-primary"}>-</button>
+//             <span style={{ padding: "10px", fontSize: "30px" }}>{counter}</span>
+//             <button onClick={() => setCounter(counter + 1)} className={"btn btn-primary"}>+</button>
+//             <br/>
+//             <div>
+//                 <button onClick={() => setConditionalUpdate(true)} className={"btn btn-primary"}>Condition based update</button>
+//             </div>
 //         </div>
 //     )
 // }
