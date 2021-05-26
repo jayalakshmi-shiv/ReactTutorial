5f96e906ab51a1b9e281f51930d6e5f30e520bb8
Jayalakshmi S <jayalakshmis692@gmail.com>
2021-05-26 15:41:19 +0530

    Class component lifecycle methods

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import Counter from './Counter'
 import './App.css';
 
 function App() {
+  const [greet, setGreet] = React.useState("Welcome Guys!!!");
+  const [counterMounted, setCounterMounted] = React.useState(true);
+  let counterContainer = "";
+
+  const onCounterUnmount = () => {
+    setCounterMounted(!counterMounted);
+  }
+
+  setTimeout(() => {
+    setGreet("I have updated from parent component")
+  }, 3000)
+
+  if (counterMounted) {
+    counterContainer = <Counter greet={greet} />;
+  }
 
   return (
-    <div className="App" style={{margin:'auto', width:'50%'}}>
-      "welcome to react session"
-      <Counter/>
+    <div className="App" style={{margin:'auto', width:'50%',textAlign: "left"}}>
+       <div>{counterContainer}</div>
+      <br />
+      <div>
+        <button onClick={onCounterUnmount} className={"btn btn-primary"} >(Un)Mount</button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
--- src/Counter.js
+++ src/Counter.js
@@ -1,38 +1,82 @@
 import React from 'react'
 
-// class Counter extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.state={counter:0}
-//     }
+class Counter extends React.Component {
+    constructor(props) {
+        console.log("constructor");
+        super(props);
+        this.state = { counter: 0 }
+    }
+    // Mounting Phase
+    componentWillMount() {                                                              //deprecated
+        console.log("component will mount");
+    }
+    componentDidMount() {
+        console.log("component did mount");
+    }
 
-//     render (){
-//         return(
-//           <div>
-//               <button>-</button>
-//               {this.state.counter}
-//               <button>+</button>
-//           </div>
-//         )
-//     }
-// }
+    //     static getDerivedStateFromProps(props, state) {
+    //         console.log("inside get derived states from props", props, state);
+    //         return null;
+    //     }
 
-// export default Counter;
+    // Update Phase
+    componentWillReceiveProps(nextProps) {                                             //deprecated
+        console.log("component will receive props", nextProps);
+    }
+
+    componentWillUpdate(nextProps, nextState) {                                        //deprecated
+        console.log("component will update", nextProps, nextState);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("should component update", nextProps, nextState);
+        // for not rendering, uncomment first line and comment second line after this
+        // if(true) return false;   
+        return true;
+    }
+    //     getSnapshotBeforeUpdate(prevProps, prevState) {
+    //         console.log("inside get snapshot before update", prevProps, prevState);
+    //         return null;
+    //     }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("component did update", prevProps, prevState);
+    }
+
+    //unmount phase
+    componentWillUnmount() {
+        console.log("inside component will unmount")
+    }
 
 
-
-const Counter = ()=>{
-
-    const [counter, setCounter] = React.useState(0);
-
-   
-    return(
-        <div>
-            <button onClick={()=>setCounter(counter-1)}>-</button>
-                {counter}
-            <button onClick={()=>setCounter(counter+1)}>+</button>
-        </div>
-    )
+    render() {
+        console.log("render")
+        return (
+            <div>
+                <h1>{this.props.greet}</h1>
+                <button className={"btn btn-primary"} onClick={() => this.setState({ counter: this.state.counter - 1 })}>-</button>
+                <span style={{padding:"10px"}}>{this.state.counter}</span>
+                <button className={"btn btn-primary"} onClick={() => this.setState({ counter: this.state.counter + 1 })}>+</button>
+            </div>
+        )
+    }
 }
 
 export default Counter;
+
+
+
+// const Counter = ()=>{
+
+//     const [counter, setCounter] = React.useState(0);
+
+//     return(
+//         <div>
+//             <button onClick={()=>setCounter(counter-1)}>-</button>
+//                 {counter}
+//             <button onClick={()=>setCounter(counter+1)}>+</button>
+//         </div>
+//     )
+// }
+
+// export default Counter;
